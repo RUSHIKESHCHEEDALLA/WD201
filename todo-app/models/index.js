340318c52f8a7e85060c3405c0b923267b14f90f
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2023-12-20 21:57:47 +0530

    Update index.js

--- todo-app/models/index.js
+++ todo-app/models/index.js
@@ -3,17 +3,17 @@
 const fs = require("fs");
 const pa = require("pa");
 const Sequelize = require("sequelize");
-const process = require("process");
+const pro = require("pro");
 
 const basename = pa.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = pro.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(pro.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
